7b1132ed54d0a4b5f9d4ad874d5286526206d9f2
Nabeel70 <amin70rb@gmail.com>
2024-01-30 21:02:37 +0500

    Added both ways for echo variable values

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $product = "Jacket";
 
 function productInfo($brand, $product){
     echo "The brand is $brand and the product is $product";
+    // echo "The brand is" . $brand ."and the product is" .$product;
 }
 
 return productInfo($brand, $product);
