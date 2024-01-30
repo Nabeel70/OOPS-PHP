7340b1c6022357e7ad901040e79a1097714544a2
Nabeel70 <amin70rb@gmail.com>
2024-01-30 20:40:56 +0500

    Setup file for start

--- index.php
+++ index.php
@@ -8,32 +8,7 @@
 <body>
     
 <?php
-// <<<<<<<<<<<<<<  ✨ Codeium Command ⭐ >>>>>>>>>>>>>>>>
-class BasicObject {
-    // Properties
-    public $name;
-    public $value;
 
-    // Constructor
-    public function __construct($name, $value) {
-        $this->name = $name;
-        $this->value = $value;
-    }
-
-    // Method to display properties
-    public function display() {
-        echo "Name: " . $this->name . ", Value: " . $this->value;
-    }
-}
-
-// Creating an instance of BasicObject
-$basicObject = new BasicObject("Sample", 42);
-
-// Calling a method of the object
-$basicObject->display();
-
-?>
-<<<<<<<  78e7fe28-a631-4909-867f-4205ec8700fc  >>>>>>>
 ?>
 </body>
 </html>
