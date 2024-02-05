e0e3b94e7b2d13654eb9e7cf07a80839702c0f1e
Nabeel70 <amin70rb@gmail.com>
2024-02-05 21:38:19 +0500

    Setup construct properties

--- Classes/Car.php
+++ Classes/Car.php
@@ -9,8 +9,10 @@ class cloth{
     private $price;
 
     //Constructor(A constructor allows you to initialize an object's properties upon creation of the object.)
-    public function __construct()
+    public function __construct($brand, $product, $price)
     {
-         
+         $this->Gucci = $brand;
+         $this->Jacket = $product;
+         $this->Price = $price;
     }
 }
